f0bd2aeb285902196bcac5cfcc9e83ecb09366d0
Himanshu Soni <himanshusoni@himanshus-macbook-air.local>
2018-06-12 21:55:13 +0530

    Completed the CRUD or completed the app using coredata with category Screen an next screen.

--- Chroma Notes/Controller/ChromaViewController.swift
+++ Chroma Notes/Controller/ChromaViewController.swift
@@ -12,6 +12,13 @@ class ChromaViewController: UITableViewController {
 
     var itemArray = [Item]()
     
+    var selectedCategory: Category? {
+        didSet {
+            loadItems()
+        }
+        
+    }
+    
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
@@ -71,6 +78,7 @@ class ChromaViewController: UITableViewController {
             let newItem = Item(context: self.context)
             newItem.title = textField.text!
             newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             
             self.saveItems()
@@ -86,9 +94,7 @@ class ChromaViewController: UITableViewController {
     
     //MARK- Model Manupulation methods
     func saveItems() {
-        
-      
-        
+
         do {
            try context.save()
             
@@ -99,8 +105,17 @@ class ChromaViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil ) {
 
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+    
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate,additionalPredicate])
+        }else {
+            request.predicate = categoryPredicate
+        }
+        
+        
         do {
         itemArray = try context.fetch(request)
         }catch {
@@ -120,11 +135,11 @@ extension ChromaViewController: UISearchBarDelegate {
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
       let request: NSFetchRequest<Item> = Item.fetchRequest()
-         request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+      let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
         
        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
 
-        loadItems(with: request)
+        loadItems(with: request, predicate: predicate)
 
         
     }
